5c5104e638e8ab06d45b11d8b96399db40b38b77
GrayofD4 <GrayofD4@users.noreply.github.com>
2017-05-22 17:15:33 -0700

    Added Mobile menu children toggle

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,6 @@ jQuery(document).ready(function($) {
 
 // MOBILE TOGGLE
 	$('#mobile-nav').slideUp(0); $('#mobile-toggle').click(function() { $( "#mobile-nav" ).slideToggle(400);console.log("Mobile Toggled"); });
+	$('#mobile-nav .menu-item-has-children .sub-menu').slideUp(0);$('#mobile-nav .menu-item-has-children > a').click(function(event) {event.preventDefault();var parentLI = $(this).parent('li');$(parentLI).toggleClass('menu-opened').children('.sub-menu').slideToggle(400);});
 
 });
