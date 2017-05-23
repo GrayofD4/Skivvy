cc4f5697abc6d3e44a9973ddcfe9d10952546ef5
GrayofD4 <GrayofD4@users.noreply.github.com>
2017-05-23 10:28:00 -0700

    Added auto-collapse menu

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,6 @@ jQuery(document).ready(function($) {
 
 // MOBILE TOGGLE
 	$('#mobile-nav').slideUp(0); $('#mobile-toggle').click(function() { $( "#mobile-nav" ).slideToggle(400);console.log("Mobile Toggled"); });
+	$('#mobile-nav .menu-item-has-children .sub-menu').slideUp(0);$('#mobile-nav .menu-item-has-children > a').click(function(event) {event.preventDefault();$('#mobile-nav .menu-opened').removeClass('menu-opened').children('.sub-menu').slideUp(400);var parentLI = $(this).parent('li');$(parentLI).addClass('menu-opened').children('.sub-menu').slideToggle(400);});
 
 });
